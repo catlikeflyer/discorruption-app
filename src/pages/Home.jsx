c17d6820a906df3799559464245a9ec815708c7e
Ace0x <59582360+Ace0x@users.noreply.github.com>
2022-03-04 15:59:44 -0600

    I'm losing it chief

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,8 @@ import ModalWhistle from "../components/ModalWhistle";
 
 export default function Home() {
   return (
-    <div className="background-image: url('https://cdn.pixabay.com/photo/2020/03/26/10/58/norway-4970080_960_720.jpg')" >
-    <div className="min-h-screen p-4 font-title bg-gradient-to-br from-[#132C33] to-[#126E82] bg-opacity-5">
+    <section className="flex items-center justify-center h-screen m-auto bg-fixed bg-cover bg-[url('https://pixabay.com/get/g4c866c7f4a1d7dc07c9cd4c7b2f55f82c137fef9d6b33e20fc0614c22a0f16f74f6c62a6a7d9b31276d1d6bfa4e2848e.jpg')]" >
+    <div className="min-h-max p-4 font-title bg-[#132C33] bg-opacity-90">
       <div className="flex flex-col justify-center items-center m-8  text-white">
         <h2 className="font-title  justify-start text-7xl">Realiza tu denuncia completamente anonima y segura.</h2>
         <p className="text-6xl">¡Por un México libre de corrupción!</p>
@@ -12,6 +12,6 @@ export default function Home() {
       </div>
       
     </div>
-    </div>
+    </section>
   );
 }
