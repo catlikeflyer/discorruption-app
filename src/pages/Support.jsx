e20c9b6f0279d3c3838c8e675f2a89ceed228ff1
Ace0x <59582360+Ace0x@users.noreply.github.com>
2022-03-05 12:53:02 -0600

    added support page
    soy la verga... lo se

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -2,9 +2,76 @@ import React from 'react'
 
 export default function Support() {
   return (
-    <div>{/**
-     * Aqui pon tu codigo 
-     * 
-     */}</div>
-  )
+    <section>
+    <div className="flex min-h-screen min-w-screen    bg-gradient-to-r from-blue-900 to-gray-900">
+      <div className="min-h-max font-title bg-transparent ">
+        <div className="flex flex-col  text-white">
+          <h2 className="font-title  text-3xl md:text-6xl text-center">
+            Preguntas frecuentes.
+          </h2>
+        </div>
+        <div className="flex-col p-10 text-white justify-center text-left">
+        
+          <h3 className=" underline md:text-3xl text-xl">
+            ¿Qué es la corrupción?
+          </h3>
+          <p className=' mb-10'>
+          Es un fenómeno social, en el cual existen condiciones sistemáticas de apropiación ilegítima del espacio público para propósitos particulares.
+Es una práctica consistente en la utilización de las funciones y medios de las organizaciones en provecho, económico o de otra índole, de sus gestores.
+          </p>
+          <h3 className="underline md:text-3xl text-xl">
+            ¿Qué es la denuncia administrativa?
+          </h3>
+          <p className='mb-10'>
+          Es un acto procesal por el que cualquier persona, por escrito o medio electrónico, ante la autoridad administrativa correspondiente, relata hechos posiblemente constitutivos de faltas administrativas cometidas por servidores públicos y, en su caso, por particulares.
+          </p>
+          <h3 className="underline md:text-3xl text-xl">
+            ¿Qué es la denuncia penal?
+          </h3>
+          <p className='mb-10'>
+          Es un acto procesal por el que cualquier persona, verbalmente o por escrito, ante el Ministerio Público relata hechos posiblemente constitutivos de delito.
+          </p>      
+          <h3 className="underline md:text-3xl text-xl">
+          ¿Quién puede denunciar?
+          </h3>
+          <p className='mb-10'>
+          Cualquier persona que tenga conocimiento de una posible falta administrativa o de un delito por hecho de corrupción cometido por algún servidor público o por una persona física o jurídica.
+          </p>
+          <h3 className="underline md:text-3xl text-xl">
+          ¿Cómo se presenta la denuncia?
+          </h3>
+          <p className='mb-10'>
+          Las denuncias se pueden presentar de manera electrónica en la página principal. Pueden ser ANÓNIMAS. En su caso, la autoridad mantendrá con carácter de confidencial la identidad del denunciante. Deberán de contener datos o indicios que permitan advertir la presunta responsabilidad.
+          </p>   
+          <h3 className="underline md:text-3xl text-xl">
+          ¿Qué debe contener la denuncia?
+          </h3>
+          <ul className="list-disc">
+            <li>En caso de no ser anónima, los datos de identificación y ubicación del denunciante.</li>
+            <li>Nombre y puesto de la servidora o servidor público denunciado.</li>
+            <li>En caso de no conocer el nombre y el puesto, hacer una descripción física de la servidora o servidor público denunciado, así como de las funciones que realiza, y del lugar o área de trabajo.</li>
+            <li>Tratándose de denuncias en contra de personas físicas o jurídicas, deberá señalarse el nombre de la persona física o de la persona jurídica, y de ser posible de las personas que las representan.</li>
+            <li>Si se desconoce el nombre de las personas físicas o jurídicas, o sus representantes, se deberá hacer una descripción física de las personas, así como de cualquier dato que permita la identificación de las mismas.</li>
+            <li>Una narración de los hechos que dieron origen a la presunta falta administrativa o delito.</li>
+            <li> En su caso, las pruebas que se tengan respecto de la falta o delito denunciado.</li>
+          </ul>
+          <h3 className="underline md:text-3xl text-xl">
+          ¿Cómo me puedo proteger mejor en línea?
+          </h3>
+          <p className='mb-10'> 
+          <ul className="list-disc">
+            <li>Usa un VPN </li>
+            <li>Activa modo incognito</li>
+            <li>Revisa los permisos que le das a tus aplicaciones</li>
+            <li>Trata de usar sistemas de mensajería encriptados</li>
+            <li>Si tratas con sitios dudosos o no confiables, instala un antivirus</li>
+          </ul>
+          </p>   
+          </div>
+         
+        
+      </div>
+      </div>
+    </section>
+  );
 }
