6f61665998100427becb1ffb7eedc694179a3b42
Do Hyun Nam <dollis2002@gmail.com>
2022-03-05 11:19:29 -0600

    Support added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Denuncias from './pages/Denuncias'
 import ScreenWhistle from './components/ScreenWhistle'
+import Support from './pages/Support'
 
 function App() {
 
@@ -24,6 +23,9 @@ function App() {
         </Routes>
         <Routes>
           <Route path="/denunciar" element={<ScreenWhistle />} />
+        </Routes>
+        <Routes>
+          <Route path="/soporte" element={<Support />} />
         </Routes>
       </Router>
     </div>
