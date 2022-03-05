173f14411bf85b93e25f347d81dab4e85c958dbe
Do Hyun Nam <dollis2002@gmail.com>
2022-03-05 08:00:37 -0600

    Added filters

--- src/pages/Denuncias.jsx
+++ src/pages/Denuncias.jsx
@@ -6,13 +6,37 @@ export default function Denuncias() {
   const { useFirestore } = useContext(DataContext);
   const { docs } = useFirestore("denuncias");
 
+  const [search, setSearch] = React.useState("");
+
+  const filteredFunc = (docs, search) =>
+    docs.filter(
+      (doc) =>
+        doc.denounced.toLowerCase().includes(search.toLowerCase()) ||
+        doc.desc.toLowerCase().includes(search.toLowerCase()) ||
+        doc.id.toLowerCase().includes(search.toLowerCase())
+    );
+
+  const filtered = docs && filteredFunc(docs, search);
+
   return (
     <div
       id="denuncias"
       className="min-h-screen items-start justify-center bg-black"
     >
       <h1 className="text-2xl text-white text-center font-title">Denuncias</h1>
-      {docs && docs.map((denuncia) => <DenunciaCard denuncia={denuncia} />)}
+      <div class="pt-2 relative mx-auto text-gray-300">
+        <input
+          class="border-2 border-gray-300 white-glassmorphism h-8 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          type="search"
+          name="search"
+          placeholder="Buscar"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      {filtered &&
+        filtered.map((denuncia) => (
+          <DenunciaCard denuncia={denuncia} key={denuncia.id} />
+        ))}
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import ModalWhistle from "../components/ModalWhistle";
-import Denuncias from "./Denuncias";
 
 export default function Home() {
   return (
     <section className="flex items-center justify-center h-screen m-auto bg-fixed bg-cover bg-gradient-to-r from-blue-900 to-gray-900">
       <div className="min-h-max p-4 font-title bg-transparent bg-opacity-90">
         <div className="flex flex-col justify-center items-center m-8  text-white">
-          <h2 className="font-title  justify-start text-7xl">
+          <h2 className="font-title justify-start text-3xl md:text-6xl ">
             Realiza tu denuncia completamente anonima y segura.
           </h2>
-          <p className="text-6xl">¡Por un México libre de corrupción!</p>
-          <ModalWhistle buttonText="Denuncia" />
+          <br />
+          <p className="md:text-4xl text-xl">
+            ¡Por un México libre de corrupción!
+          </p>
+          <ModalWhistle buttonText="Denunciar" />
+          <a
+            style={{ textDecoration: "none" }}
+            className="md:hidden"
+            href="/denunciar"
+          >
+            <div
+              className="button-container bg-blue-700 m-10 hover:bg-blue-500 text-white font-bold py-2 px-4 border border-blue-700 md:hidden"
+              href="/denunciar"
+            >
+              Denunciar
+            </div>
+          </a>
         </div>
       </div>
     </section>
